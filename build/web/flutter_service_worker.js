1ecd722bafd4e0169eb3d134903da5098b276928
Enzobits1 <enzost25@gmail.com>
2025-06-15 12:48:04 -0300

    main

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -37,7 +37,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "403cb7a9c96054c5aa1944fa4295c37f
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "d7a5cfd01dea8e6ec6994de630c30c74",
+"flutter_bootstrap.js": "c12700f8abb1cfc3cb43eae72e300ea2",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
